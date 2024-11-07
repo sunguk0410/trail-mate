c3e4bd52dc1193a55fb74049e281eb97a877cd8d
sunguk <kkcc4104@naver.com>
2024-11-07 20:22:47 +0900

    bottomsheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.material:material:1.9.0")
+
 }
--- app/src/main/java/com/example/trail_mate/MapsFragment.java
+++ app/src/main/java/com/example/trail_mate/MapsFragment.java
@@ -1,6 +1,7 @@
 package com.example.trail_mate;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,27 +17,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
+    private BottomSheetBehavior<View> bottomSheetBehavior;
     private GoogleMap mMap;
 
     public MapsFragment() {
         // Required empty public constructor
-    }
-
-    public static MapsFragment newInstance(String param1, String param2) {
-        MapsFragment fragment = new MapsFragment();
-        Bundle args = new Bundle();
-        args.putString("param1", param1);
-        args.putString("param2", param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -54,6 +43,42 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
+        }
+
+        // BottomSheet 초기화
+        View bottomSheet = view.findViewById(R.id.bottomSheet);
+        if (bottomSheet != null) {
+            bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+            // 드래그 활성화
+            bottomSheetBehavior.setDraggable(true);
+
+            // 초기 상태 설정
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+            // BottomSheet 상태 변경 리스너 추가
+            bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                    if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                        Log.d("BottomSheet", "BottomSheet expanded");
+                    } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                        Log.d("BottomSheet", "BottomSheet collapsed");
+                    } else if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                        Log.d("BottomSheet", "BottomSheet dragging");
+                    } else if (newState == BottomSheetBehavior.STATE_SETTLING) {
+                        Log.d("BottomSheet", "BottomSheet settling");
+                    } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                        Log.d("BottomSheet", "BottomSheet hidden");
+                    }
+                }
+
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    // 드래그 또는 슬라이드 중일 때 호출됩니다. slideOffset은 0.0 (축소)에서 1.0 (완전히 확장) 사이입니다.
+                    Log.d("BottomSheet", "BottomSheet sliding: " + slideOffset);
+                }
+            });
         }
     }
 
