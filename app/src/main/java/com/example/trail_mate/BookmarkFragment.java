2d876b8d544bb1102d6ee24d85eccf52000b60e0
sunguk <kkcc4104@naver.com>
2024-11-08 00:10:03 +0900

    viewmodel 구현

--- app/src/main/java/com/example/trail_mate/BookmarkFragment.java
+++ app/src/main/java/com/example/trail_mate/BookmarkFragment.java
@@ -1,48 +1,24 @@
 package com.example.trail_mate;
 
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.trail_mate.databinding.FragmentBookmarkBinding;
 
 public class BookmarkFragment extends Fragment {
-    private FragmentBookmarkBinding binding;
-    private SharedViewModel sharedViewModel;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // ViewBinding 초기화
-        binding = FragmentBookmarkBinding.inflate(inflater, container, false);
-
-        // SharedViewModel 초기화
-        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-
-        // 버튼 클릭 시 SharedViewModel에 데이터 추가
-        binding.buttonFragment1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = binding.editTextFromFragment1.getText().toString();
-                if (!text.isEmpty()) {
-                    sharedViewModel.addLocation(text);
-                    binding.editTextFromFragment1.setText(""); // 입력 필드 초기화
-                }
-            }
-        });
-
-        return binding.getRoot();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null; // 메모리 누수 방지
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bookmark, container, false);
     }
 }
